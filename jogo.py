06d40ba2024aa608d4e31f2bcbbb12d1cb4a0f92
raulrangel <pidapunia90@gmail.com>
2022-05-23 10:00:45 -0300

    Voltando para o plano de fundo

--- jogo.py
+++ jogo.py
@@ -44,15 +44,8 @@ TILE_parede = pygame.image.load('assets\parede.png').convert_alpha()
 # TILE_parede = pygame.transform.scale(TILE_parede, (TILE_WIDTH, TILE_HEIGHT))
 TILE_chao = pygame.image.load('assets\chao.png').convert_alpha()
 # TILE_chao = pygame.transform.scale(TILE_chao, (TILE_WIDTH, TILE_HEIGHT))
-# background = pygame.image.load('assets\planodefundo.png').convert()
-
-def cria_mapa(tamanhox,tamanhoy):
-    mapa = []
-    for a in range(tamanhox):
-        mapa.append([])
-        for b in range(tamanhoy):
-            mapa[a].append(random.randint(0,1))
-    return mapa
+background = pygame.image.load('assets\planodefundo.png').convert()
+background = pygame.transform.scale(background, (WIDTH,HEIGHT))
 
 def message_new_round():
     clock = pygame.time.Clock()
@@ -73,26 +66,6 @@ def message_new_round():
         pygame.display.flip()
         clock.tick(5)
                     
-
-rr = cria_mapa(TILES_HORIZONTAL,TILES_VERTICAL)
-mapa1 = [[0 if bbbb<3 or bbbb>16 else 1 for aaaa in range(TILES_HORIZONTAL)] for bbbb in range(TILES_VERTICAL)]
-
-# for a in mapa1:
-#     print(a)
-# print(mapa1)
-
-def printa_mapa(mapa,chao,parede,tamanho):
-    contx, conty = 0, 0
-    for a in range(len(mapa)):
-        for b in range(len(mapa)):
-            if mapa[b][a] == 0:
-                window.blit(parede,(contx,conty))
-            else:
-                window.blit(chao,(contx,conty))
-            contx += tamanho
-        conty += tamanho
-        contx = 0
-
 # ----- Inicia estruturas de dados
 class jogador(pygame.sprite.Sprite):
     def __init__(self, img):
@@ -299,9 +272,9 @@ while game:
         game = False
 
     # ----- Gera saídas
-    window.fill(WHITE)  # Preenche com a cor branca
-    # window.blit(background,(0,0)) # Coloca o background
-    printa_mapa(mapa1,TILE_chao,TILE_parede,TILE_RESOLUTION)
+    # window.fill(WHITE)  # Preenche com a cor branca
+    window.blit(background,(0,0)) # Coloca o background
+    # printa_mapa(mapa1,TILE_chao,TILE_parede,TILE_RESOLUTION)
     sprites.draw(window)
     enemies.draw(window)
     moedas.draw(window)
